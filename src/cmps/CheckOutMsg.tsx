f43699f7972facab19b92adad5792c1df2290271
Lian Skopis <lian.skopis@gmail.com>
2021-06-14 00:26:55 +0300

    bug with link solved

--- src/cmps/CheckOutMsg.tsx
+++ src/cmps/CheckOutMsg.tsx
@@ -38,7 +38,7 @@ const CheckOutMsg: React.FC<Props> = ({invoiceTotalForMsg}) =>{
                 <Typography variant="body2" component="pre" style={{marginTop: '16px'}}>
                 <p>Your items are due to arrive within the next 7 business days</p>
                 <p>Feel free to contact us for any questions or issues At:</p>
-                <a className="contact-link" href="my-store@react.redux">my-store@react.redux</a>
+                <a className="contact-link" href="">my-store@react.redux</a>
                 </Typography>
             </CardContent>
         </Card>
